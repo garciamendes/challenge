9a992792e265de501476c7c003dc0fec66d105dc
garciamendes <garciaprog0101@gmail.com>
2025-01-31 14:01:17 -0300

    feat: base project configuration

---
+++ backend/src/env/index.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import { z as zod } from "zod";
+
+const envSchema = zod.object({
+  NODE_ENV: zod.enum(["dev", "test", "production"]).default("dev"),
+  JWT_SECRET: zod.string(),
+  DATABASE_URL: zod.string(),
+  SALT_HASH: zod.coerce.number().default(10),
+  ALLOW_CORS: zod.string(),
+  PORT: zod.coerce.number().default(3333),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.error("❌ Invalid environment variables", _env.error.format());
+  throw new Error("Invalid environment variables");
+}
+
+export const env = _env.data;
---
+++ backend/src/server.ts
@@ -0,0 +1,42 @@
+import Fastify from "fastify";
+import { ZodError } from "zod";
+import { env } from "./env";
+import fastifyJwt from "@fastify/jwt";
+import fastifyCors from "@fastify/cors";
+
+export const fastify = Fastify();
+
+fastify.register(fastifyCors, {
+  origin: env.ALLOW_CORS.split(","),
+  methods: ["GET", "PUT", "PATCH", "POST", "DELETE"],
+  allowedHeaders: ["content-type", "accept", "content-type", "authorization"],
+});
+fastify.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  sign: {
+    expiresIn: "7d",
+  },
+});
+
+fastify.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation error", issue: error.format() });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  }
+
+  reply.status(500).send({ message: "Internal Server Error" });
+});
+
+fastify
+  .listen({
+    host: "0.0.0.0",
+    port: env.PORT || 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server Running 🚀");
+  });
